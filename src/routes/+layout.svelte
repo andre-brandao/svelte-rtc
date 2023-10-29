531658ecbc95d74b9ebb5e79108fa950048ee601
André de Oliveira Brandão <brandaoandre00@gmail.com>
2023-10-29 03:36:18 -0300

    new main page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,15 @@
 <script>
 	import '../app.postcss';
+	import { AppBar } from '@skeletonlabs/skeleton';
+	import { Tv } from 'lucide-svelte';
 </script>
+
+<AppBar>
+	<svelte:fragment slot="lead">
+		<Tv />
+	</svelte:fragment>
+	<svelte:fragment slot="trail">(actions)</svelte:fragment>
+	<svelte:fragment slot="headline">Fake Meet</svelte:fragment>
+</AppBar>
 
 <slot />
